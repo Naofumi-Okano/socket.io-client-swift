27a4c8cc16a1771f6bce8b9847e57c627ab20bf3
Naofumi-Okano <okano-naofumi@cyber-z.co.jp>
2022-11-14 17:48:15 +0900

    Add Target

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -6,5 +6,10 @@ let package = Package(
     name: "SocketIO-Legacy",
     products: [
         .library(name: "SocketIO-Legacy", targets: ["SocketIO-Legacy"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "SocketIO-Legacy"),
     ]
 )
